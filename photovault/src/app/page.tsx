b130fb894e9ca7ab131903e02a8c21738b9c4d9e
drekuu <drekuuu@gmail.com>
2024-11-06 00:10:01 +0100

    Fix eslint warning

--- photovault/src/app/page.tsx
+++ photovault/src/app/page.tsx
@@ -1,10 +1,4 @@
-'use client';
-
-import { useTranslations } from 'next-intl';
-
 export default function Home() {
-  const t = useTranslations('HomePage');
-
   return (
     <div>
       <h1>Home page</h1>
